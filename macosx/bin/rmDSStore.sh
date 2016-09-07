afb4c594cffcf3e57477043ab8164ca75c31d617
zhenghaiyong <zhenghaiyong@gmail.com>
2016-09-07 17:29:48 +0800

    Modify to delete ._* files

--- macosx/bin/rmDSStore.sh
+++ macosx/bin/rmDSStore.sh
@@ -1,1 +1,2 @@
 find ~ \( -path ~/.subversion -o -path ~/workspace/www \) -prune -o -name ".DS_Store" -print -exec rm {} \;
+find ~ \( -path ~/.subversion -o -path ~/workspace/www \) -prune -o -name "._*" -print -exec rm {} \;
